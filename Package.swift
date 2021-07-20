a1d87d20e333a8e309c0abc90896b70b5215abfd
Muhammad Firman Nur <muhammad.firman@bukalapak.com>
2021-07-20 16:12:39 +0700

    Try update package

--- Package.swift
+++ Package.swift
@@ -2,26 +2,75 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+let headersSearchPath: [CSetting] = [.headerSearchPath("."),
+                                     .headerSearchPath("Base"),
+                                     .headerSearchPath("Debug"),
+                                     .headerSearchPath("Details"),
+                                     .headerSearchPath("Details/Transactions"),
+                                     .headerSearchPath("Layout"),
+                                     .headerSearchPath("Private"),
+                                     .headerSearchPath("Private/Layout"),
+                                     .headerSearchPath("TextExperiment/Component"),
+                                     .headerSearchPath("TextExperiment/String"),
+                                     .headerSearchPath("TextExperiment/Utility"),
+                                     .headerSearchPath("TextKit"),
+                                     .headerSearchPath("tvOS"),]
+
+let sharedDefines: [CSetting] = [
+                                // Disable "old" textnode by default for SPM
+                                .define("AS_ENABLE_TEXTNODE", to: "0"),
+    
+                                // PINRemoteImage always available for Texture
+                                .define("AS_PIN_REMOTE_IMAGE", to: "1"),
+                                
+                                // always disabled
+                                .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
+
+func IGListKit(enabled: Bool) -> [CSetting] {
+    let state: String = enabled ? "1" : "0"
+    return [
+        .define("AS_IG_LIST_KIT", to: state),
+        .define("AS_IG_LIST_DIFF_KIT", to: state),
+    ]
+}
+
+
 let package = Package(
-    name: "AsyncDisplayKit",
-    platforms: [.iOS(.v9)],
+    name: "Texture",
+    platforms: [
+             .macOS(.v10_15),
+             .iOS(.v10),
+             .tvOS(.v10)
+         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AsyncDisplayKit",
+            type: .static,
             targets: ["AsyncDisplayKit"]),
+        .library(
+            name: "AsyncDisplayKitIGListKit",
+            type: .static,
+            targets: ["AsyncDisplayKitIGListKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.2"),
+        .package(url: "https://github.com/Instagram/IGListKit", from: "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-                    name: "AsyncDisplayKit",
-                    url: "https://github.com/CodeEagle/Texture/releases/download/3.0.0/AsyncDisplayKit.xcframework.zip",
-                    checksum: "6059f04c8b52ee1e36782c524c0cb21f33640ef399400a02e751a625b3bcb93a"
-                )
-    ]
+        .target(
+            name: "AsyncDisplayKit",
+            dependencies: ["PINRemoteImage"],
+            path: "spm/Sources/AsyncDisplayKit",
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: false)
+        ),
+        .target(
+            name: "AsyncDisplayKitIGListKit",
+            dependencies: ["IGListKit", "PINRemoteImage"],
+            path: "spm/Sources/AsyncDisplayKitIGListKit/AsyncDisplayKit",
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: true)
+        ),
+    ],
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx11
 )
